64b4f0c349f21b6afd8e94efb047e5c3f37829df
javierdejesusda <javier.dejesusj9@gmail.com>
2026-04-12 03:13:16 +0200

    dashboard: move ticker row to the very top of the shell

--- dashboard/src/components/shell/AppShell.tsx
+++ dashboard/src/components/shell/AppShell.tsx
@@ -9,9 +9,9 @@ import { TopBar } from "./TopBar";
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col bg-[color:var(--color-paper)]">
+      <TickerTape />
       <ApiHealthBanner />
       <TopBar />
-      <TickerTape />
       <div className="flex flex-1 min-h-0">
         <LeftRail />
         <main className="flex-1 min-w-0 overflow-auto">
